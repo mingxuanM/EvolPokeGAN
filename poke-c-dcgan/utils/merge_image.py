61c6e06cc03df9572e3b36d9116fbbb28f601d35
Steve <steve@StevedeMacBook-Pro.local>
2019-03-19 17:50:35 +0000

    test data

--- poke-c-dcgan/utils/merge_image.py
+++ poke-c-dcgan/utils/merge_image.py
@@ -16,9 +16,7 @@ def crop_im(im):
     crop_rectangle = (5, 0, 35, 30)
     return im.crop(crop_rectangle)
 
-def blank_image():
-    im = Image.new('RGB', (120,120), color=0)
-    return im
+
 
 csv_file = pd.read_csv('../data/pokemon-dex-ev.csv')
 ev_idxs = csv_file.iloc[:, 32].values
@@ -27,25 +25,28 @@ pre_idxs = csv_file.iloc[:, 33].values
 
 idx_pairs = list(zip(ev_idxs, pre_idxs))
 
-for p in idx_pairs:
-    if not math.isnan(p[1]):
-        im_pair = []
-        im1 = Image.open(img_dir + '%03d' % int(p[0]) + '.png')
-        im2 = Image.open(img_dir + '%03d' % int(p[1]) + '.png')
-        # im1 = crop_im(im2)
-        # im2 = crop_im(im2)
-        im_pair.append(im1)
-        im_pair.append(im2)
-        widths, heights = zip(*(i.size for i in im_pair))
-        total_width = sum(widths)
-        max_height = max(heights)
 
-        new_im = Image.new('RGB', (total_width, max_height))
-        x_offset = 0
-        for im in im_pair:
-            new_im.paste(im, (x_offset,0))
-            x_offset += im.size[0]
-        new_im.save(save_dir + '/' + '%03d' % int(p[0]) + '-ev-r10' + '.png')
+
+# Merge evol pairs
+# for p in idx_pairs:
+#     if not math.isnan(p[1]):
+#         im_pair = []
+#         im1 = Image.open(img_dir + '%03d' % int(p[0]) + '.png')
+#         im2 = Image.open(img_dir + '%03d' % int(p[1]) + '.png')
+#         # im1 = crop_im(im2)
+#         # im2 = crop_im(im2)
+#         im_pair.append(im1)
+#         im_pair.append(im2)
+#         widths, heights = zip(*(i.size for i in im_pair))
+#         total_width = sum(widths)
+#         max_height = max(heights)
+
+#         new_im = Image.new('RGB', (total_width, max_height))
+#         x_offset = 0
+#         for im in im_pair:
+#             new_im.paste(im, (x_offset,0))
+#             x_offset += im.size[0]
+#         new_im.save(save_dir + '/' + '%03d' % int(p[0]) + '-ev-r10' + '.png')
 
 
 # images = []
