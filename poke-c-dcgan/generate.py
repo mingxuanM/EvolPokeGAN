dac7afa168ee950baccb59f4a04f194be82907bc
Steve <steve@StevedeMacBook-Pro.local>
2019-03-21 19:28:31 +0000

    generate

--- poke-c-dcgan/generate.py
+++ poke-c-dcgan/generate.py
@@ -11,10 +11,10 @@ import json
 from model import Generator
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-load_path', default='checkpoint/model_final.pth', help='Checkpoint to load path from')
+parser.add_argument('-load_path', default='checkpoints-pokedex/model_500.pth', help='Checkpoint to load path from')
 parser.add_argument('-num_output', default=9, help='Number of generated outputs')
-parser.add_argument('-eye_color', default='blue', help='Eye color')
-parser.add_argument('-hair_color', default='blonde', help='Hair color')
+parser.add_argument('-tp1', default='fire', help='Eye color')
+parser.add_argument('-tp2', default='<UNK>', help='Hair color')
 parser.add_argument('-load_json', default='data/animegan_params.json', help='Load path for params json.')
 args = parser.parse_args()
 
@@ -24,7 +24,7 @@ state_dict = torch.load(args.load_path)
 # Load color2ind dictionary from json parameter file.
 with open(args.load_json, 'r') as info_file:
             info = json.load(info_file)
-            color2ind = info['color2ind']
+            tp2ind = info['tp2ind']
 
 # Set the device to run on: GPU or CPU.
 device = torch.device("cuda:0" if(torch.cuda.is_available()) else "cpu")
@@ -46,23 +46,30 @@ onehot = torch.zeros(params['vocab_size'], params['vocab_size'])
 onehot = onehot.scatter_(1, torch.LongTensor([i for i in range(params['vocab_size'])]).view(params['vocab_size'], 1), 1).view(params['vocab_size'], params['vocab_size'], 1, 1)
 
 # Create input conditions vectors.
-input_condition = torch.cat((torch.ones(int(args.num_output), 1)*color2ind[args.eye_color], 
-                            torch.ones(int(args.num_output), 1)*color2ind[args.hair_color]),
+input_condition = torch.cat((torch.ones(int(args.num_output), 1)*color2ind[args.tp1], 
+                            torch.ones(int(args.num_output), 1)*color2ind[args.tp2]),
                             dim=1).type(torch.LongTensor)
 
 # Generate the onehot embeddings for the conditions.
-eye_ohe = onehot[input_condition[:, 0]].to(device)
-hair_ohe = onehot[input_condition[:, 1]].to(device)
+tp1_ohe = onehot[input_condition[:, 0]].to(device)
+tp2_ohe = onehot[input_condition[:, 1]].to(device)
 
 # Turn off gradient calculation to speed up the process.
 with torch.no_grad():
     # Get generated image from the noise vector using
     # the trained generator.
-    generated_img = netG(noise, eye_ohe, hair_ohe).detach().cpu()
+    generated_img = netG(noise, tp1_ohe, tp2_ohe).detach().cpu()
 
 # Display the generated image.
 plt.axis("off")
 #plt.title("Generated Images")
-plt.imshow(np.transpose(vutils.make_grid(generated_img, nrow=int(np.sqrt(int(args.num_output))), padding=2, normalize=True), (1,2,0)))
+img_data = np.transpose(vutils.make_grid(fake_data, nrow=6, padding=2, normalize=True).cpu(), (1, 2, 0))
+plt.imsave(result_dir +'/Epoch_{}.png'.format(epoch), img_data)
 
-plt.show()
+result_dir = 'test_result/' + args.load_path.split('/')[1].split('.')[0]
+if not os.path.exists(result_dir):
+    os.mkdir(result_dir)
+    print("Directory " , result_dir ,  " Created ")
+
+img_data = np.transpose(vutils.make_grid(generated_img, nrow=int(np.sqrt(int(args.num_output))), padding=2, normalize=True), (1,2,0))
+plt.imsave(result_dir +'/re.png', img_data)
